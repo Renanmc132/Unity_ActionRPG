a1b6036fcdb743d547520ceb7f3653d709cd7013
Renanmc132 <renanmc100@gmail.com>
2026-06-17 20:17:01 -0300

    feat : Adiciona movimentação ao player

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,17 +1,21 @@
+using UnityEditor.Experimental.GraphView;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
 
     private float moveSpeed = 5f;
-    private Rigidbody2D rb;
+    private Rigidbody2D _rb;
+    private Animator _anim;
+    private int direction = 1;
 
 
 
 
     void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();
+        _rb = GetComponent<Rigidbody2D>();
+        _anim = GetComponent<Animator>();
     }
 
 
@@ -21,9 +25,25 @@ public class PlayerMovement : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        rb.linearVelocity = new Vector2(horizontalInput,verticalInput) * moveSpeed;
+        _anim.SetFloat("xInput", Mathf.Abs(horizontalInput));
+        _anim.SetFloat("yInput", Mathf.Abs(verticalInput));
+
+        if (horizontalInput > 0 && direction != 1 || horizontalInput < 0 && direction != -1)
+            Flip();
+
+        _rb.linearVelocity = new Vector2(horizontalInput,verticalInput) * moveSpeed;
 
 
 
     }
+
+
+    private void Flip()
+    {
+        direction *= -1;
+        transform.localScale = new Vector3(direction,transform.localScale.y,transform.localScale.z);
+    }
+
+
+
 }
